84623cee37341c436232f5b9eee3f874ced86ae8
akhilesh <akhileshgowda7@gmail.com>
2022-07-09 19:23:48 -0400

    Created post create component

--- MEAN-Project/src/app/posts/post-create/post-create.component.ts
+++ MEAN-Project/src/app/posts/post-create/post-create.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-post-create',
-  templateUrl:'./post-create.component.html'
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
 })
-export class PostCreateComponent{
-
+export class PostCreateComponent {
+  newpost = '';
+  enteredValue = '';
+  onAddPost() {
+    this.newpost = this.enteredValue;
+  }
 }
