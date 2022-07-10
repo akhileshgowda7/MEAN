322c100feb6f67c0b8af760777b8b3c03819d7fd
akhilesh <akhileshgowda7@gmail.com>
2022-07-10 18:31:09 -0400

    Adding the post dynamically

--- MEAN-Project/src/app/posts/post-create/post-create.component.ts
+++ MEAN-Project/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -6,9 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  newpost = '';
-  enteredValue = '';
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter();
+
   onAddPost() {
-    this.newpost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent,
+    };
+    console.log(`when post created `);
+    // console.log(post)
+    this.postCreated.emit(post);
   }
 }
--- MEAN-Project/src/app/posts/post-list/post-list.component.ts
+++ MEAN-Project/src/app/posts/post-list/post-list.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {}
+export class PostListComponent {
+  // posts = [
+  //   {
+  //     title: 'First Post',
+  //     content: 'This is the first post',
+  //   },
+  //   {
+  //     title: 'Second Post',
+  //     content: 'This is the Second post',
+  //   },
+  //   {
+  //     title: 'First Third',
+  //     content: 'This is the Third post',
+  //   },
+  // ];
+  @Input() posts = [];
+}
